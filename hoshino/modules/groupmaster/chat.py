0393c9e9dcd3562b465f0090991ee7f15a59a658
jyk714017560 <714017560@qq.com>
2020-07-13 23:17:59 +0800

    色图功能提醒

--- hoshino/modules/groupmaster/chat.py
+++ hoshino/modules/groupmaster/chat.py
@@ -53,6 +53,10 @@ async def nihaole(bot, ev):
     await bot.send(ev, '不许好，憋回去！')
     await util.silence(ev, 30)
 
+@sv.on_fullmatch(('色图', '涩图','瑟图' ))
+async def setu(bot, ev):
+    await bot.send(ev, f"色图功能升级中……\n{R.img('setu.jpg').cqcode}")
+
 
 # ============================================ #
 
