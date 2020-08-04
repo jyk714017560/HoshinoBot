64284bf46cbe99ad0f9bd525935a48cb259a1c82
jyk714017560 <714017560@qq.com>
2020-08-04 23:47:34 +0800

    同步hoshinobot改动

--- hoshino/modules/priconne/query/whois.py
+++ hoshino/modules/priconne/query/whois.py
@@ -21,13 +21,15 @@ async def whois(bot, ev: CQEvent):
         return
     id_ = chara.name2id(name)
     confi = 100
+    guess = False
     if id_ == chara.UNKNOWN:
         id_, guess_name, confi = chara.guess_id(name)
+        guess = True
     c = chara.fromid(id_)
     
     msg = ''
-    if confi < 100:
-        msg = f'兰德索尔似乎没有叫"{name}"的人...'
+    if guess:
+        msg = f'兰德索尔似乎没有叫"{name}"的人...\n角色别称补全计划: github.com/Ice-Cirno/HoshinoBot/issues/5'
         await bot.send(ev, msg)
         msg = f'您有{confi}%的可能在找{guess_name} '
 
