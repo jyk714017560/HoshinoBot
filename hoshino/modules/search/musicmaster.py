97dab4bc645b190914524879eb81b40353b9ea49
jyk714017560 <714017560@qq.com>
2020-09-06 11:23:30 +0800

    feature:更换卡池、签到盖章、点歌功能优化

--- hoshino/modules/search/musicmaster.py
+++ hoshino/modules/search/musicmaster.py
@@ -13,12 +13,22 @@ from hoshino import logger
 session = requests.session()
 
 def music_keyword(keyword):
-    keywordUrl = 'http://musicapi.leanapp.cn/search'
-    musicUrl = 'https://api.imjad.cn/cloudmusic/'
+    url = 'http://music.163.com/api/search/pc'
+    headers = {
+    'Cookie': 'appver = 2.0.2',
+    'refer': 'http://music.163.com',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'
+    }
+    params = {
+    's': keyword,
+    'offset': 0,
+    'limit': 1,
+    'type': 1
+    }
     try:
-        keywordResult = session.get(url=keywordUrl, params={'keywords': keyword}, timeout=10)
+        keywordResult = session.post(url=url, headers=headers, params=params, timeout=10)
     except (requests.exceptions.RequestException) as e:
-        logger.error(f'[musicapi.leanapp.cn connect failed]{e}')
+        logger.error(f'[music.163.com connect failed]{e}')
         return '点歌失败惹 QAQ\n有可能是服务器网络爆炸，请重试一次'
     
     keywordJson = keywordResult.json()
@@ -26,21 +36,13 @@ def music_keyword(keyword):
         id = keywordJson['result']['songs'][0]['id']
         title = keywordJson['result']['songs'][0]['name']
         artist = keywordJson['result']['songs'][0]['artists'][0]['name']
+        image = keywordJson['result']['songs'][0]['album']['blurPicUrl']
+        imageUrl = f'{image}?param=90y90'
+        audioUrl = f'http://music.163.com/song/media/outer/url?id={id}.mp3'
     else:
         return '没有版权，发不出去勒...'
-    
-    try:
-        musicResult = session.get(url=musicUrl, params={'id': id}, timeout=10)
-    except (requests.exceptions.RequestException) as e:
-        logger.error(f'[api.imjad.cn connect failed]{e}')
-        return '点歌失败惹 QAQ\n有可能是服务器网络爆炸，请重试一次'
-    
-    musicJson = musicResult.json()
-    audioUrl = musicJson['data'][0]['url']
-    if not audioUrl:
-        return '没有版权，发不出去勒...'
-    
-    return MessageSegment.music_custom(url='https://game.granbluefantasy.jp/', audio_url=audioUrl, title=title, content=artist)
+  
+    return MessageSegment.music_custom(url='https://bh3.mihoyo.com/', audio_url=audioUrl, title=title, content=artist, image_url=imageUrl)
 
 
 class MusicThread(threading.Thread):
