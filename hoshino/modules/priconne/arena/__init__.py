3d58d7731c39c58fd903865e62f1e0d52759b779
jyk714017560 <714017560@qq.com>
2025-06-03 11:41:52 +0800

    提交最近的优化

--- hoshino/modules/priconne/arena/__init__.py
+++ hoshino/modules/priconne/arena/__init__.py
@@ -1,9 +1,10 @@
 import re
 import time
 from collections import defaultdict
+from PIL import Image, ImageDraw, ImageFont
 
 import hoshino
-from hoshino import Service
+from hoshino import Service, R
 from hoshino.typing import *
 from hoshino.util import FreqLimiter, concat_pic, pic2b64
 
@@ -17,13 +18,21 @@ sv_help = '''
 sv = Service('pcr-arena', help_=sv_help, bundle='pcr查询')
 
 from . import arena
-
+fontPath = R.img('font/msyhbd.ttf').path
 lmt = FreqLimiter(5)
 
 aliases = ('怎么拆', '怎么解', '怎么打', '如何拆', '如何解', '如何打', 'jjc查询')
 aliases_b = tuple('b' + a for a in aliases) + tuple('B' + a for a in aliases)
 aliases_tw = tuple('台' + a for a in aliases)
 aliases_jp = tuple('日' + a for a in aliases)
+
+try:
+    thumb_up_i = R.img('priconne/gadget/thumb-up-i.png').open().resize((16, 16), Image.LANCZOS)
+    thumb_up_a = R.img('priconne/gadget/thumb-up-a.png').open().resize((16, 16), Image.LANCZOS)
+    thumb_down_i = R.img('priconne/gadget/thumb-down-i.png').open().resize((16, 16), Image.LANCZOS)
+    thumb_down_a = R.img('priconne/gadget/thumb-down-a.png').open().resize((16, 16), Image.LANCZOS)
+except Exception as e:
+    sv.logger.exception(e)
 
 @sv.on_prefix(aliases)
 async def arena_query(bot, ev):
@@ -77,32 +86,46 @@ async def _arena_query(bot, ev: CQEvent, region: int):
 
     # 执行查询
     sv.logger.info('Doing query...')
-    res = await arena.do_query(defen, uid, region)
+    res = None
+    try:
+        res = await arena.do_query(defen, uid, region)
+    except hoshino.aiorequests.HTTPError as e:
+        code = e.response["code"]
+        if code == 117 or code == -429:
+            await bot.finish(ev, "高峰期服务器限流！请前往pcrdfans.com/battle")
+        else:
+            await bot.finish(ev, f'code{code} 查询出错，请联系维护组调教\n请先前往pcrdfans.com进行查询', at_sender=True)
     sv.logger.info('Got response!')
 
     # 处理查询结果
     if res is None:
-        await bot.finish(ev, '查询出错，请联系维护组调教\n请先移步pcrdfans.com进行查询', at_sender=True)
+        await bot.finish(ev, '数据库未返回数据，请再次尝试查询或前往pcrdfans.com', at_sender=True)
     if not len(res):
         await bot.finish(ev, '抱歉没有查询到解法\n※没有作业说明随便拆 发挥你的想象力～★\n作业上传请前往pcrdfans.com', at_sender=True)
     res = res[:min(6, len(res))]    # 限制显示数量，截断结果
 
     # 发送回复
     sv.logger.info('Arena generating picture...')
-    atk_team = [ chara.gen_team_pic(entry['atk']) for entry in res ]
-    atk_team = concat_pic(atk_team)
+    m = await bot.get_group_member_info(
+        self_id=ev.self_id, group_id=ev.group_id, user_id=ev.user_id
+    )
+    atk_team = render_atk_def_teams(res)
     atk_team = pic2b64(atk_team)
-    atk_team = str(MessageSegment.image(atk_team))
+    atk_team = MessageSegment.image(atk_team)
+    # atk_team = [ chara.gen_team_pic(entry['atk']) for entry in res ]
+    # atk_team = concat_pic(atk_team)
+    # atk_team = pic2b64(atk_team)
+    # atk_team = str(MessageSegment.image(atk_team))
     sv.logger.info('Arena picture ready!')
     # 纯文字版
     # atk_team = '\n'.join(map(lambda entry: ' '.join(map(lambda x: f"{x.name}{x.star if x.star else ''}{'专' if x.equip else ''}" , entry['atk'])) , res))
 
-    details = [ " ".join([
-        f"赞{e['up']}+{e['my_up']}" if e['my_up'] else f"赞{e['up']}",
-        f"踩{e['down']}+{e['my_down']}" if e['my_down'] else f"踩{e['down']}",
-        e['qkey'],
-        "你赞过" if e['user_like'] > 0 else "你踩过" if e['user_like'] < 0 else ""
-    ]) for e in res ]
+    # details = [ " ".join([
+    #     f"赞{e['up']}+{e['my_up']}" if e['my_up'] else f"赞{e['up']}",
+    #     f"踩{e['down']}+{e['my_down']}" if e['my_down'] else f"踩{e['down']}",
+    #     e['qkey'],
+    #     "你赞过" if e['user_like'] > 0 else "你踩过" if e['user_like'] < 0 else ""
+    # ]) for e in res ]
 
     defen = [ chara.fromid(x).name for x in defen ]
     defen = f"防守方【{' '.join(defen)}】"
@@ -112,9 +135,9 @@ async def _arena_query(bot, ev: CQEvent, region: int):
         defen,
         f'已为骑士{at}查询到以下进攻方案：',
         str(atk_team),
-        f'作业评价：',
-        *details,
-        '※发送"点赞/点踩"可进行评价'
+        # f'作业评价：',
+        # *details,
+        # '※发送"点赞/点踩"可进行评价'
     ]
     if region == 1:
         msg.append('※使用"b怎么拆"或"台怎么拆"可按服过滤')
@@ -148,3 +171,29 @@ async def _arena_feedback(bot, ev: CQEvent, action:int):
     except KeyError:
         await bot.finish(ev, '无法找到作业id！您只能评价您最近查询过的作业', at_sender=True)
     await bot.send(ev, '感谢您的反馈！', at_sender=True)
+
+
+def render_atk_def_teams(entries, border_pix=5):
+    n = len(entries)
+    icon_size = 64
+    im = Image.new('RGBA', (5 * icon_size + 100, n * (icon_size + border_pix) - border_pix), (255, 255, 255, 255)).convert("RGBA")
+    font = ImageFont.truetype(fontPath, 16)
+    draw = ImageDraw.Draw(im)
+    for i, e in enumerate(entries):
+        y1 = i * (icon_size + border_pix)
+        y2 = y1 + icon_size
+        for j, c in enumerate(e['atk']):
+            icon = c.render_icon(icon_size)
+            x1 = j * icon_size
+            x2 = x1 + icon_size
+            im.paste(icon, (x1, y1, x2, y2), icon)
+        thumb_up = thumb_up_a if e['user_like'] > 0 else thumb_up_i
+        thumb_down = thumb_down_a if e['user_like'] < 0 else thumb_down_i
+        x1 = 5 * icon_size + 5
+        x2 = x1 + 16
+        im.paste(thumb_up, (x1, y1+22, x2, y1+38))
+        im.paste(thumb_down, (x1, y1+44, x2, y1+60))
+        draw.text((x1, y1), e['qkey'], (0, 0, 0, 255), font)
+        draw.text((x1+16, y1+20), f"{e['up']}+{e['my_up']}" if e['my_up'] else f"{e['up']}", (0, 0, 0, 255), font)
+        draw.text((x1+16, y1+40), f"{e['down']}+{e['my_down']}" if e['my_down'] else f"{e['down']}", (0, 0, 0, 255), font)
+    return im
--- hoshino/modules/priconne/dailymusic/__init__.py
+++ hoshino/modules/priconne/dailymusic/__init__.py
@@ -1,12 +1,21 @@
+import requests
 import random
+import time
+
+try:
+    import ujson as json
+except:
+    import json
 
 import hoshino
-from hoshino import Service,R
+from hoshino import Service,R,logger
 from hoshino.typing import *
 
 from . import _music_data
+from . import _vacation_data
 
 sv = Service('dailymusic', help_='', bundle='pcr娱乐', enable_on_default=True, visible=False)
+session = requests.session()
 
 def music_gener():
     musiclist = []
@@ -33,3 +42,51 @@ async def daily_music():
     music = MessageSegment.music(type_=musicInfo[0], id_=musicId)
     await bot.send_group_msg(group_id=602138153, message='\n'.join(msg))
     await bot.send_group_msg(group_id=602138153, message=music)
+
+@sv.scheduled_job('cron', hour=19, minute=00)
+async def daily_music():
+    bot = hoshino.get_bot()
+    msg=[]
+    today=time.time()
+    try:
+        for vacationInfo in _vacation_data.VACATION_DATA.values():
+            holiday=''
+            vDay=time.mktime(time.strptime(vacationInfo[0],'%Y-%m-%d'))
+            intervalDay=int((vDay-today)/(60*60*24))
+            if intervalDay<=0:
+                continue
+            if vacationInfo[2]==1:
+                holiday='假期'
+            msg.append(f'距离【{vacationInfo[1]}】{holiday}还有:{intervalDay}天')
+        msg.append("\n接下来为mmk大人献上精挑细选的晚间音乐~")
+        #msg.append(f"{R.img('打工人.jpg').cqcode}")
+        await bot.send_group_msg(group_id=873875268, message='\n'.join(msg))
+        music=mmk_music()
+        await bot.send_group_msg(group_id=873875268, message=music)
+    except Exception as e:
+        logger.error(f'{e}')
+
+
+def mmk_music():
+    url = 'https://music.163.com/api/v1/playlist/detail'
+    headers = {
+    'Cookie': 'appver = 2.0.2',
+    'refer': 'http://music.163.com',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'
+    }
+    params = {
+        'id': 315624195
+    }
+    try:
+        keywordResult = session.post(url=url, headers=headers, params=params, timeout=10)
+    except (requests.exceptions.RequestException) as e:
+        logger.error(f'[music.163.com connect failed]{e}')
+        return '没有版权，发不出去勒...'
+    
+    keywordJson = keywordResult.json()
+    if keywordJson['playlist']['trackIds']:
+        id=random.choice(keywordJson['playlist']['trackIds'])['id']
+    else:
+        return '没有版权，发不出去勒...'
+  
+    return MessageSegment.music(type_='163', id_=id)
